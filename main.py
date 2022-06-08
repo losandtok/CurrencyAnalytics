4a1173c597a3c4e36b34ab736c5d848b2f97c0aa
losandtok <105535596+losandtok@users.noreply.github.com>
2022-06-08 15:33:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from fastapi import FastAPI
+import secrets
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 
+security = HTTPBasic()
 
 
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "losandtok")
+    correct_password = secrets.compare_digest(credentials.password, "yan432678")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-@app.get("/plus/{x}/{y}")
-async def sum(x, y):
-    return {int(x) +int(y)}
-@app.get("/multiply/{x}/{y}")
-async def sum(x, y):
-    return {int(x) * int(y)}
-@app.get("/difference/{x}/{y}")
-async def difference(x,y):
-    return {int(x) - int(y)}
+@app.get("/users/me")
+def read_current_user(username: str = Depends(get_current_username)):
+    return {"username": username} 
