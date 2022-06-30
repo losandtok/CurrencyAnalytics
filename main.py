023f80e46b593f2e6c0afd369516ae18dcc08823
losandtok <105535596+losandtok@users.noreply.github.com>
2022-06-30 18:19:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ security = HTTPBasic()
 
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "losandtok")
-    correct_password = secrets.compare_digest(credentials.password, "yan432678")
+    correct_username = secrets.compare_digest(credentials.username, "yan")
+    correct_password = secrets.compare_digest(credentials.password, "2508")
     if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
