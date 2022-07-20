8793cb9ba92574c85e95c2b7f095eda1744b292c
losandtok <105535596+losandtok@users.noreply.github.com>
2022-07-20 13:59:10 +0200

    Add files via upload

--- timeseries_rates.py
+++ timeseries_rates.py
@@ -1,21 +1,6 @@
 
-import requests
 
 
-def set_timeseries(start_date, end_date):
-    url = f"https://api.apilayer.com/exchangerates_data/timeseries?start_date={start_date}&end_date={end_date}&base=USD"
-
-    payload = {}
-    headers = {
-        "apikey": "nqVSc2eE3eoDiOYk3szQYL1ZQ5kPDBXH"
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    result = response.text
-    with open(r"timeseries_rates.txt", 'w') as file:
-        file.seek(0)
-        file.write(result)
 
 
 def translate_date(start_day, start_month, start_year, end_day, end_month, end_year):
