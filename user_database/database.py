06cd18b669051db15b20494c4a2dcb454b1cbdf3
losandtok <losandtok@gmail.com>
2022-07-27 20:31:09 +0200

    Added Postgres database

--- user_database/database.py
+++ user_database/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+SQLALCHEMY_DATABASE_URI = 'postgres://yxjakgazhvvxvb:aac65fdcf984547be34bcb4df35456b752b01fc050fb2cf9a4bbd6178bcf4171@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/dbadgq9revtsgg'
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
