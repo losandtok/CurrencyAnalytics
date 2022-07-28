d49bd4d6c2fc10184c757e515192d54e6429a13b
losandtok <losandtok@gmail.com>
2022-07-28 13:53:32 +0200

    bug with comparing password

--- user_database/database.py
+++ user_database/database.py
@@ -3,11 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URI = 'postgres://yxjakgazhvvxvb:aac65fdcf984547be34bcb4df35456b752b01fc050fb2cf9a4bbd6178bcf4171@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/dbadgq9revtsgg'
 
+SQLALCHEMY_DATABASE_URL  = 'postgresql+psycopg2://yxjakgazhvvxvb:aac65fdcf984547be34bcb4df35456b752b01fc050fb2cf9a4bbd6178bcf4171@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/dbadgq9revtsgg'
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
